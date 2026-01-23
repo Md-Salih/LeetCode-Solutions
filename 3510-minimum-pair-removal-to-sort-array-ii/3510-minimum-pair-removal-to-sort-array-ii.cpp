e51b5db5ee7332cef83a4bd6d5ad61c133e810e1
Md-Salih <mdsalih.m2005@gmail.com>
2026-01-23 22:25:01 +0530

    Time: 376 ms (100%), Space: 111.9 MB (100%) - LeetHub

--- 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.cpp
+++ 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.cpp
@@ -1,16 +1,11 @@
-// this version uses long long packed data instead of pair int2
 using ll=long long;
 using int2=pair<long long, int>;
 const int N=1e5;
 bitset<N> rmv=0;
 int prv[N], nxt[N];
-ll heap[3*N];// this is changed
+int2 heap[3*N];
 class Solution {
 public:
-    // pack function 
-    static inline ll pack(ll sum, unsigned idx){
-        return ((ll)sum<<17)|idx;
-    }
     static int minimumPairRemoval(vector<int>& nums_) {
         const int n=nums_.size();
         ll* nums=(ll*)alloca(n*sizeof(ll));
@@ -26,7 +21,7 @@ public:
         }
 
         for (int i=0; i<n-1; i++)
-            heap[i]=pack(nums[i]+nums[i+1], i);
+            heap[i]={nums[i]+nums[i+1], i};
         int sz=n-1;
         make_heap(heap, heap+sz, greater<>());
 
@@ -37,12 +32,9 @@ public:
 
         int op=0;
 
-        while (bad>0 && sz>0) {
+        while (bad>0) {
             pop_heap(heap, heap+sz, greater<>());
-            ll data=heap[--sz];
-            // unpack data into sum & i
-            ll sum=data>>17;
-            int i=data&((1<<17)-1);
+            auto [sum, i]=heap[--sz];
 
             if (rmv[i] || nxt[i]==-1) continue;
             int j=nxt[i];
@@ -67,13 +59,13 @@ public:
             if (pi!=-1 && nums[pi]>nums[i]) bad++;
             if (nj!=-1 && nums[i]>nums[nj]) bad++;
 
-            // update heap using pack
+            // update heap
             if (pi != -1){
-                heap[sz++]=pack(nums[pi]+nums[i], pi);
+                heap[sz++]={nums[pi]+nums[i], pi};
                 push_heap(heap, heap+sz, greater<>());
             }
             if (nj != -1){
-                heap[sz++]=pack(nums[i]+nums[nj], i);
+                heap[sz++]={nums[i]+nums[nj], i};
                 push_heap(heap, heap+sz, greater<>());
             }
             op++;
@@ -82,3 +74,4 @@ public:
         return op;
     }
 };
+
