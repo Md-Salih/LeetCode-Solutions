707c3d1541c8249fa9ff13cfac90640677ea3f25
Md-Salih <mdsalih.m2005@gmail.com>
2026-02-05 20:07:05 +0530

    Time: 4 ms (79.67%), Space: 25.7 MB (70.75%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> constructTransformedArray(vector<int>& A) {
-        int n = A.size();
+        int n = A.size(), bias = n * (99 / n) + n;
         
         vector<int> res;
         for (int i = 0; i < n; i++)
-            res.push_back(A[(((i + A[i]) % n) + n) % n]);
+            res.push_back(A[(i + A[i] + bias) % n]);
 
         return res;
     }
